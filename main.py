b5186a29d81986f618b7571e7c230fde99c3ca26
stautuan <stautuan89@gmail.com>
2024-08-19 13:27:02 -0400

    render player on screen

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import pygame
 
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
     
     while True:
@@ -13,9 +15,11 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-    
-        screen.fill("black")
 
+        # fill the screen black
+        screen.fill("black")
+        # render the player on screen
+        player.draw(screen)
         # refresh the screen
         pygame.display.flip()
 
